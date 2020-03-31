df17e6463adf7c21a5f3832298e5024bef4d6a58
Armando Baños Pascual <abpprkonsalting@gmail.com>
2020-03-30 23:44:14 -0400

    removed Pravams Giftcard module

--- app/code/Pravams/Giftcard/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*' : {
-            'Magento_Tax/js/view/checkout/summary/grand-total' : 'Pravams_Giftcard/js/view/checkout/summary/grand-total'
-        }
-    }
-};
